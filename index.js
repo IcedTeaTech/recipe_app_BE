3968a843617c26df24b0ec32f861e61fd35f4c6d
IcedTeaTech <icedteatech@gmail.com>
2020-11-05 10:12:02 +0100

    first version

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+const app = express();
+const port = 4000;
+
+const APP_ID = `${process.env.APP_ID}`;
+const API_KEY = `${process.env.API_KEY}`;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.post("/recipes", async (req, res) => {
+  let { searchQuery } = req.body;
+  console.log(req.body);
+  const response = await axios
+    .get(
+      `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${API_KEY}`
+    )
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port 4000");
+});
